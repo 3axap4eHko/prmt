17da34d7c34c4929bd480679679914dfa41d794c
Ivan Zakharchanka <3axap4eHko@gmail.com>
2025-08-18 10:05:47 -0400

    formatting

--- src/modules/time.rs
+++ src/modules/time.rs
@@ -12,14 +12,14 @@ impl Default for TimeModule {
 impl Module for TimeModule {
     fn render(&self, format: &str, _context: &ModuleContext) -> Option<String> {
         let now = Local::now();
-        
+
         let formatted = match format {
             "12h" | "12H" => now.format("%I:%M%p"),
             "12hs" | "12HS" => now.format("%I:%M:%S%p"),
             "24hs" | "24HS" => now.format("%H:%M:%S"),
             _ => now.format("%H:%M"),
         };
-        
+
         Some(formatted.to_string())
     }
 }
@@ -48,13 +48,13 @@ mod tests {
         let result_default = module.render("", &context);
         assert!(result_default.is_some());
         assert_eq!(result_default.unwrap().len(), 5);
-        
+
         let result_12hs = module.render("12hs", &context);
         assert!(result_12hs.is_some());
         let time_12hs = result_12hs.unwrap();
         assert!(time_12hs.contains(':'));
         assert!(time_12hs.ends_with("AM") || time_12hs.ends_with("PM"));
-        
+
         let result_24hs = module.render("24hs", &context);
         assert!(result_24hs.is_some());
         let time_24hs = result_24hs.unwrap();
